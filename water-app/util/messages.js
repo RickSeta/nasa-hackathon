2e82696d122b26f2226a70e4ef442df637daed0d
Arch <33100644+mttorres@users.noreply.github.com>
2023-10-08 02:41:01 -0300

    feat: Message and Quizz utils.

---
+++ water-app/util/messages.js
@@ -0,0 +1,23 @@
+import { ptbr_msgs } from "./ptbr";
+import { en_msgs } from "./en";
+
+class MessageService {
+
+    #msgs = {
+        'ptbr': ptbr_msgs,
+        'en':  en_msgs,
+    }
+
+    getLocale() {
+        return 'ptbr';
+    }
+
+    getMessage(key) {
+        return this.#msgs[this.getLocale()][key]
+    }
+
+
+}
+
+let messageService = Object.freeze(new MessageService());
+export default messageService;
