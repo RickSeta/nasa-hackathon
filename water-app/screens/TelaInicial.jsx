b8cb10b279059814afc96201a31474cfc549696e
Arch <33100644+mttorres@users.noreply.github.com>
2023-10-07 23:14:18 -0300

    feat: Navigation to Decision Screen.

--- water-app/screens/TelaInicial.jsx
+++ water-app/screens/TelaInicial.jsx
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, Text, View } from 'react-native';
 import Buttonn from '../components/button';
+import { useNavigation } from "@react-navigation/native";
 import GridComponent from '../components/fourbutton';
 import Fourbutton from '../components/fourbutton';
 import Grid from '../components/fourbutton';
 
 export default function TelaInicial() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on your app!</Text> */}
       {/* <StatusBar style="auto" /> */}
       {/* <Buttonn color='red' text={'Botao parametrizado'} callback={()=>{alert('Pode botar qualquer função aqui!!')}}/> */}
-      <Grid color='red' text={'Botao parametrizado'} callback={()=>{alert('Pode botar qualquer função aqui!!')}}/>
+      <Grid color='red' text={'Botao parametrizado'} callback={() => navigation.navigate('TelaDecisao')}/>
       {/* <Fourbutton></Fourbutton> */}
     </View>
   );
